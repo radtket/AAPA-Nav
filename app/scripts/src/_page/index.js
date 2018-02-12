20780f5e06720f65e3457af5f3d935c37da34762
Taylor Radtke <tradtke@aapa.org>
2018-02-12 11:10:00 -0500

    review

--- app/scripts/src/_page/index.js
+++ app/scripts/src/_page/index.js
@@ -17,8 +17,30 @@ $(window).load(function() {
 
 $(document).ready(function() {
 	$(window).trigger('resize');
+	init_classic_menu();
 });
 
 $(window).resize(function() {
 	// js_height_init();
 });
+
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	nav: false,
+	items: 1,
+	autoplay: true,
+	autoplayTimeout: 7000,
+	autoplayHoverPause: true,
+});
+
+function init_classic_menu() {
+	$('.nav__menu-item--depth0').click(function(e) {
+		if ($('.header__tertiary').hasClass('header__tertiary--active')) {
+			$('.header__tertiary').removeClass('header__tertiary--active');
+			$(this).removeClass('current-menu-item');
+		} else {
+			$('.header__tertiary').addClass('header__tertiary--active');
+			$(this).addClass('current-menu-item');
+		}
+	});
+}
